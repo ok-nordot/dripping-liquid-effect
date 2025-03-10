c6dfa5f12e328dfafc24d1a2dd21725ea00906a4
ok-nordot <ok@nordot.jp>
2025-03-10 22:53:24 +0900

    netlify plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { vitePlugin as remix } from "@remix-run/dev";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { vitePlugin as remix } from '@remix-run/dev'
+import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import { netlifyPlugin } from '@netlify/remix-adapter/plugin'
 
-declare module "@remix-run/node" {
+declare module '@remix-run/node' {
   interface Future {
-    v3_singleFetch: true;
+    v3_singleFetch: true
   }
 }
 
@@ -20,5 +21,6 @@ export default defineConfig({
       },
     }),
     tsconfigPaths(),
+    netlifyPlugin(),
   ],
-});
+})
